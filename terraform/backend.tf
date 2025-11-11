4b2876a0d50442ff6371b6b4d1422fc444b523fc
siddharthaSailsLearning <gcplearning.2025.sre@gmail.com>
2025-11-11 12:02:23 +0530

    added bucket

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.5.0"
 
   backend "gcs" {
-    bucket = "placeholder-bucket-overrided-by-cipipeline"
-    prefix = "placeholder-prefix-overrided-by-cipipeline"
+    bucket = "pipeline-tfstate-bucket-for-infra-mgmt"
+    prefix = "terraform/state"
   }
 }
